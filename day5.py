dfbe71d6cf7caaef7c36f8566d4e3e4f24814b48
Mishra <mishras8@mail.uc.edu>
2024-12-05 14:03:05 -0500

    Advent of Code Day 5 Part 2 using recursion :D

--- day5.py
+++ day5.py
@@ -39,7 +39,7 @@ for i in range(index_of_newline + 1, len(lines)):
             break
 
 count = 0
-print(pages) # logging the pages that are valid 
+#print(pages) # logging the pages that are valid 
 
 # find the mid index of each valid page update and sum it up
 for i in pages:
@@ -47,3 +47,44 @@ for i in pages:
     count+= i[int(mid_index)]
 print(count)
 
+# PART 2 
+
+# swap 2 numbers functions
+def swap(a,b):
+    temp = a
+    a = b
+    b = temp
+    return a,b
+
+# function to swap nth and n-1th element to correct the order
+def correct(x,rules,result):
+    for i in range(1,len(x)):
+        if [x[i],x[i-1]] in rules:
+            x[i],x[i-1] = swap(x[i],x[i-1])
+            # use recursion to keep swapping until correct order is achieved 
+            correct(x,rules,result)
+        else:
+            continue
+        if x not in result:
+            result.append(x)
+    return result
+
+# corrected order list initialized
+corrected = []
+
+for i in range(index_of_newline + 1, len(lines)):
+    #parse and clean
+    line = lines[i].split(",")
+    line = [int(item.strip()) for item in line]
+    # call correct() function and pass the arguments and a list of corrected orders is returned
+    correct(line,rules,corrected)
+
+corrected_count = 0
+# sum the element in the middle of the each order
+for i in corrected:
+    mid_index = (len(i)-1)/2
+    corrected_count += i[int(mid_index)]
+print(corrected_count)
+
+
+f.close()
