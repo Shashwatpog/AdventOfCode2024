98ed0fe4142cfb22f24ffe7a40e12b1b02f1ba58
Shashwat <mishras8@mail.uc.edu>
2024-12-19 14:29:22 +0530

    Day 19 part 1 using recursion with memoization using functools cache

---
+++ day19.py
@@ -0,0 +1,26 @@
+from functools import cache
+
+patterns = []
+
+with open("input.txt","r") as f:
+    towels = f.readline().strip().split(", ")
+    f.readline()
+    for line in f.readlines():
+        patterns.append(line.strip())
+
+# using memoization with functools cache
+@cache
+def no_of_arrangements(pattern,start=0):
+    if start >= len(pattern):
+        return 1
+    result = 0
+    for i in towels:
+        if pattern[start:start+len(i)] == i:
+            result += no_of_arrangements(pattern,start+len(i)) # recursion with memoization
+    return result
+
+# Part 1 answer
+print(sum(no_of_arrangements(pattern)>0 for pattern in patterns))
+
+
+
